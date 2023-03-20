3c6ba7b5d000765423d68f3f6b779d4cc79a0e8d
Store_Builder <huzaifasattar89@gmail.com>
2023-03-20 15:40:05 +0500

    imort responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ResponsiveAppBar from './commponents/navbar/navbar-com';
 
 function App() {
   return (
     <div className="App">
-    
+    <ResponsiveAppBar/>
     </div>
   );
 }
